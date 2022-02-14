c24bca18be8ab5aeca9fc671022c08c97909d05b
Noé Delcroix <noedelcroix@hotmail.com>
2022-02-14 16:51:08 +0100

    Premier rendu

--- Stratego/Board.hpp
+++ Stratego/Board.hpp
@@ -3,6 +3,7 @@
 #include "optional"
 #include "array"
 #include "fstream"
+#include "vector"
 #include <Piece.hpp>
 #include <Position.hpp>
 #include <Direction.hpp>
@@ -10,8 +11,15 @@
 namespace Modele{
     class Board {
         static const unsigned BOARD_SIZE = 10;
+        bool cheatMode;
+        std::vector<std::pair<Piece, int>> listOfPieces;
         std::array<std::array<std::optional<Piece>,BOARD_SIZE>,BOARD_SIZE> board;
+
         void initializeArmy(std::string fileName);
+        void move(Position pos, Direction direction, int distance=1);
+        bool isGameOver();
+        int getWinner();
+        std::optional<Piece> at(Position pos);
         std::optional<Piece> attack(std::optional<Piece> piece, std::optional<Piece> piece2);
         void swapPieces(Position pos1, Position pos2);
         bool isInside(Position pos);
@@ -19,8 +27,6 @@ namespace Modele{
         void setPiece(Position pos, std::optional<Piece>);
         public:
         Board(std::string fileName);
-        void move(Position pos, Direction direction, int distance=1);
-        std::optional<Piece> getPiece(Position pos);
     };
 };
 #endif // BOARD_H
