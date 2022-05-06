5ae5d73f9b3d3ce326ca97f3df25155d02c82961
Noé DELCROIX <noedelcroix@hotmail.com>
2022-05-06 12:34:33 +0200

    change font labels

--- Stratego/gui/controller.cpp
+++ Stratego/gui/controller.cpp
@@ -7,7 +7,7 @@ namespace strategoGui{
         view.show();
         view.controller(this);
         facade.start();
-        view.updateGameStatus("You can swap your pieces. Make an invalid swap to finish.");
+        view.updateGameStatus("You can swap your pieces. \nMake an invalid swap to finish.");
         view.updateCurrentPlayer("Player " + QString::number(facade.getCurrentPlayer()) + " turn.");
     }
 }
