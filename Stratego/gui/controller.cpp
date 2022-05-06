89dc896113c636f051d6838b01ec458d0570bff0
Noé DELCROIX <noedelcroix@hotmail.com>
2022-05-06 11:36:07 +0200

    Add Swapping

--- Stratego/gui/controller.cpp
+++ Stratego/gui/controller.cpp
@@ -6,59 +6,9 @@ namespace strategoGui{
     void Controller::start(){
         view.show();
         view.controller(this);
-        facade.nextPlayer();
-        //view.update("move");
-    }
-    void Controller::move(Position pos1, Position pos2){
-        qDebug("ok");
-        qDebug("okok");
-        if(!facade.isPiece(pos1) || facade.at(pos1).getPlayer()!=facade.getCurrentPlayer()){
-            view.updateGameStatus("Cette pièce n'appartient pas au joueur.");
-            return;
-        }
-
-        view.updateGameStatus("");
-        Direction dir;
-        int dirVertical=pos1.getX()-pos2.getX();
-        int dirHorizontal=pos1.getY()-pos2.getY();
-        if(dirVertical>0)
-            dir=Direction::TOP;
-        else if(dirVertical<0){
-            dir=Direction::BOTTOM;
-        }else
-            if(dirHorizontal<0){
-                dir=Direction::RIGHT;
-            }
-            else if(dirHorizontal>0)
-                dir=Direction::LEFT;
-            else{
-                view.updateGameStatus("Cannot determinate the direction.");
-                return;
-            }
-
-
-        int distance;
-        if(dir==Direction::TOP || dir== Direction::BOTTOM){
-            distance = abs(dirVertical);
-        }else if(dir==Direction::LEFT || dir==Direction::RIGHT){
-            distance = abs(dirHorizontal);
-        }else{
-            return;
-        }
-        if(distance==0){
-            view.updateGameStatus("You have to move.");
-            return;
-        }
-        try {
-            facade.move(pos1,dir,distance);
-            facade.nextPlayer();
-            facade.isGameOver();
-        }  catch (std::exception &e) {
-            view.updateGameStatus(e.what());
-        }
-
+        facade.start();
+        view.updateGameStatus("You can swap your pieces. Make an invalid swap to finish.");
         view.updateCurrentPlayer("Player " + QString::number(facade.getCurrentPlayer()) + " turn.");
     }
-
 }
 
--- Stratego/gui/view.cpp
+++ Stratego/gui/view.cpp
@@ -23,38 +23,9 @@ View::View(Facade &f, QWidget *parent)
     this->setWindowTitle("Stratego - Smolinski Piotr & Noé Delcroix");
 
     facade.addObserver(this);
-    board.setController(ctrl_);
     board.updateBoard();
 }
-/*
-void View::dragEventHandler(bool status){
-    qDebug("dragStatus");
-}
 
-void View::eventHandler(Position pos){
-    qDebug("%zu", buttonsClicked.size());
-    if(buttonsClicked.size()==0 || pos.getX()!=buttonsClicked.at(0).getX() || pos.getY()!=buttonsClicked.at(0).getY()){
-        qDebug("ok");
-    buttonsClicked.push_back(pos);
-    }
-    if(buttonsClicked.size()>=2 && !dragStatus){
-        while(buttonsClicked.size()>2){
-            buttonsClicked.erase(buttonsClicked.begin()+1);
-        }
-        ctrl_->move(buttonsClicked.at(0),buttonsClicked.at(1));
-        //Si le deuxième emplacement n'est pas une pièce ou si la deuxième pièce n'appartient pas au joueur courant
-        if(!facade.isPiece(buttonsClicked.at(1)) || facade.at(buttonsClicked.at(1)).getPlayer()!=facade.getCurrentPlayer()){
-        buttonsClicked.clear();
-        }else{
-            buttonsClicked.erase(buttonsClicked.begin());
-        }
-    }else if(buttonsClicked.size()==1 && (!facade.isPiece(pos) || facade.at(pos).getPlayer()!=facade.getCurrentPlayer())){
-        //Si le premier emplacement cliqué n'est pas une pièce ou n'appartient pas au joueur.
-        updateGameStatus("Cette pièce n'appartient pas au joueur.");
-        buttonsClicked.clear();
-    }
-}
-*/
 void View::updateGameStatus(QString text){
     ui->gamestatus->setText(text);
 }
@@ -86,7 +57,6 @@ void View::update(const std::string & propertyName){
 
 void View::resizeEvent(QResizeEvent *e){
     ui->gridLayoutWidget->setFixedSize(ui->centralwidget->width()*0.5,ui->centralwidget->height());
-    //ui->verticalLayoutWidget->setGeometry(ui->centralwidget->width()/2,0,ui->gridLayoutWidget->width(),ui->verticalLayoutWidget->height());
     ui->verticalLayoutWidget->setFixedSize(ui->centralwidget->width()*0.5,ui->centralwidget->height());
 
 }
@@ -94,7 +64,7 @@ void View::resizeEvent(QResizeEvent *e){
 
 View::~View()
 {
-    //delete ui;
+    delete ui;
 }
 
 }
